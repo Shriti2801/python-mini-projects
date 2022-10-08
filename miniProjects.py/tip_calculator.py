c949592d4b2d70c5ae003473e2c2dc48a51b3a07
Shriti <shriti2018@gmail.com>
2022-10-08 19:32:35 +0530

    Tip Calculator

--- miniProjects.py/tip_calculator.py
+++ miniProjects.py/tip_calculator.py
@@ -5,4 +5,5 @@ tip = int(input("How much tip would you like to give? 10, 12, or 15?\n"))
 people = int(input("How many people to split the bill?\n"))
 bill_with_tip = tip/100 * bill + bill
 final_amount = round(bill_with_tip,2)
+final_amount = "{:.2f}".format(bill_with_tip)
 print("The bill per person is",final_amount)
