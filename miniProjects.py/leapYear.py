df443f7c23eff7df9a7bac99bb25ae71dcd5800d
Shriti <shriti2018@gmail.com>
2022-10-09 16:26:03 +0530

    heads or tails using randomization

--- miniProjects.py/leapYear.py
+++
@@ -1,9 +0,0 @@
-year = int(input("Which year do you want to check?\n"))
-if year % 4 == 0:
-    print("Leap year.")
-elif year % 100 ==0:
-    print("Not leap year.")
-elif year % 400 ==0:
-    print("Leap year.")
-else:
-    print("Not leap year.")
