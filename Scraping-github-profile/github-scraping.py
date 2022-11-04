f43e1d738e250332aea6cc4b574575e8fef0cc0c
Shriti Singh <SHRITI2018@GMAIL.COM>
2022-11-05 00:41:06 +0530

    Profile picture scraper

---
+++ Scraping-github-profile/github-scraping.py
@@ -0,0 +1,13 @@
+import requests
+from bs4 import BeautifulSoup as bs
+
+github_profile = "https://github.com/Shriti2801"
+req = requests.get(github_profile)
+scraper = bs(req.content,"html.parser")
+profile_picture = scraper.find("img",{"alt":"Avatar"})
+["src"]
+print(profile_picture)
+
+
+
+
